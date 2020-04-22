efe65476ff730d46cfaf7dce13cb9056f3e19d6a
Alejandro Gorrin <alejandro.gorrin.24@gmail.com>
2020-04-22 17:13:24 -0300

    cambio en src leds.c

--- src/leds.c
+++ src/leds.c
@@ -23,7 +23,7 @@ void leds_Off(uint8_t led){
     *leds &= ~leds_Bits(led);
 }
 void leds_status(uint8_t led){
-    if (*leds = 1){
+    if (*leds |= leds_Bits(led)){
         return 1;
     }
     else{
